b34cf391d314c3f9af9be894aebbd07c11bcd165
rawreef <Daniel@Daniels-MacBook-Pro.local>
2015-10-21 15:28:34 +0300

    Initial commit
    Linking Ember to Firebase

--- config/environment.js
+++ config/environment.js
@@ -4,6 +4,8 @@ module.exports = function(environment) {
   var ENV = {
     modulePrefix: 'firebase-ember-1',
     environment: environment,
+    contentSecurityPolicy: { 'connect-src': "'self' https://auth.firebase.com wss://*.firebaseio.com" },
+    firebase: 'https://ember-1-firebase.firebaseio.com/',
     baseURL: '/',
     locationType: 'auto',
     EmberENV: {
